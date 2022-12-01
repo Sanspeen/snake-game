c4a63cf145ccf28880aca0c2d81e2033c45c2b30
Santiago Franco <ktns0930@gmail.com>
2022-12-01 13:19:43 -0500

    Code has been refactorized adding Snake class to divide responsabilities.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time
+from Snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,31 +8,13 @@ screen.bgcolor("black")
 screen.title("SNAKE GAME")
 screen.tracer(0)
 
-
-def look_left(turtle):
-    turtle.left(90)
-
-
-body_parts = []
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-for position in starting_positions:
-    square = Turtle()
-    square.penup()
-    square.shape("square")
-    square.color("white")
-    square.goto(position)
-    body_parts.append(square)
+snake = Snake()
 
 screen.update()
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.2)
-    for body_part_num in range(len(body_parts) - 1, 0, -1):
-        new_x = body_parts[body_part_num - 1].xcor()
-        new_y = body_parts[body_part_num - 1].ycor()
-        body_parts[body_part_num].goto(new_x, new_y)
-    body_parts[0].forward(20)
+    snake.snake_move()
 
 screen.exitonclick()
