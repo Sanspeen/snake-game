55b9e3e161c05306e044a7db96f284cde4a35623
Santiago Franco <ktns0930@gmail.com>
2022-12-01 14:29:40 -0500

    Movement control system is ready.

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from turtle import Screen, Turtle
 import time
-from Snake import Snake
-
+from snake import Snake
+SNAKE_SPEED = 0.1
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("SNAKE GAME")
 screen.tracer(0)
+screen.listen()
 
 snake = Snake()
+screen.onkey(snake.look_up, "Up")
+screen.onkey(snake.look_down, "Down")
+screen.onkey(snake.look_left, "Left")
+screen.onkey(snake.look_right, "Right")
+
 
 screen.update()
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(SNAKE_SPEED)
     snake.snake_move()
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+
+class Snake:
+    def __init__(self):
+        self.body_parts = []
+        self.create_snake()
+        self.head = self.body_parts[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            square = Turtle()
+            square.penup()
+            square.shape("square")
+            square.color("white")
+            square.goto(position)
+            self.body_parts.append(square)
+
+    def snake_move(self):
+        for body_part_num in range(len(self.body_parts) - 1, 0, -1):
+            new_x = self.body_parts[body_part_num - 1].xcor()
+            new_y = self.body_parts[body_part_num - 1].ycor()
+            self.body_parts[body_part_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def look_up(self):
+        if round(self.head.heading()) != DOWN:
+            self.head.setheading(UP)
+
+    def look_down(self):
+        if round(self.head.heading()) != UP:
+            self.head.setheading(DOWN)
+
+    def look_left(self):
+        if round(self.head.heading()) != RIGHT:
+            self.head.setheading(LEFT)
+
+    def look_right(self):
+        if round(self.head.heading()) != LEFT:
+            self.head.setheading(RIGHT)
